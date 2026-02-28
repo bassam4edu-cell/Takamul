2acac78eb1468b43742bdd295d36005b50d9a22c
bassam4edu-cell <bassam4edu@gmail.com>
2026-02-28 20:55:02 +0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -13,7 +13,7 @@ const Logo: React.FC<LogoProps> = ({ className = "w-12 h-12", showText = false, 
   return (
     <div className={`flex flex-col items-center gap-4 ${className}`}>
       <img 
-        src="https://i.ibb.co/QFwrvnqF/photo-2026-02-20-15-16-20.jpg" 
+        src="https://i.ibb.co/ZzPTFG3y/222.png" 
         alt="شعار وزارة التعليم" 
         className="w-full h-full object-contain"
         referrerPolicy="no-referrer"
